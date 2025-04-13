fix gradients, clipping

cs336_basics/train.py
@@ -59,7 +59,8 @@ def gradient_clipping(params, max_l2_norm, eps=1e-6):
     grads = []
     for param in params:
         if param.grad is not None:
-            grads.append(param.grad)
+            # print(param.grad.shape)
+            grads.append(param.grad.view(-1))
 
     all_grads = torch.cat(grads, dim=0)
 

cs336_basics/transformer.py
@@ -31,7 +31,7 @@ class Linear(nn.Module):
         # print(self.weights.data.shape)
         # print(x.shape)
         # print('-------------------------------------------')
-        return einsum(self.weight.data.T, x, "in_features out_features, ... in_features -> ... out_features")
+        return einsum(self.weight.T, x, "in_features out_features, ... in_features -> ... out_features")
 
 class Embedding(nn.Module):
     def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
@@ -87,7 +87,7 @@ class RMSNorm(nn.Module):
         # print(rms.shape, x.shape)
         # print(self.weights / rms)
 
-        result = einsum(self.weight.data, x / torch.unsqueeze(rms,-1), "d_model, batch_size sequence_length d_model -> batch_size sequence_length d_model")
+        result = einsum(self.weight, x / torch.unsqueeze(rms,-1), "d_model, batch_size sequence_length d_model -> batch_size sequence_length d_model")
     
         return result.to(in_dtype)
     
dcf11b07b175c9312e4673253e9f97b8922df2ae
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-15 09:23:08 -0700

    evaluate more models

--- cs336_basics/eval_models.py
+++ cs336_basics/eval_models.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
 
     for model_name in model_paths: 
         # Load the saved state dict
-        saved_state_dict = torch.load(f'{args.save_dir}/{model_name}/final.pt')
+        saved_state_dict = torch.load(f'{args.save_dir}/{model_name}/iteration430000.pt')
         saved_state_dict = saved_state_dict['model']
 
         # Create a new state dict with modified keys
